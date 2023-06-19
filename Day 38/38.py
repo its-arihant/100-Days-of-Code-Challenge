f88bd34e14d0a9e980d4342c28f9833eae4aedb6
18arihant <arihant18082003@gmail.com>
2023-06-19 14:22:30 +0530

    Day 38

--- Day 38/38.py
+++ Day 38/38.py
@@ -19,7 +19,7 @@ def prime(n):
         return False
     
     i=2
-    while(i*i<n):
+    while(i*i<=n):
         if (n%i==0):
             return False
         i+=1
@@ -28,3 +28,21 @@ def prime(n):
 n=int(input("Enter a number:"))
 print(prime(n))
 # Time complexity = O(square root(n))
+
+#3rd approach(super efficient)
+def prime(n):
+    if (n==1):
+        return False
+    if (n==2 or n==3):
+        return True
+    if (n%2==0 or n%3==0):
+        return False
+    
+    i=5
+    while(i*i<=n):
+        if (n%i==0 or n%(i+2)==0):
+            return False
+        i+=6
+    return True
+n=int(input("Enter a number:"))
+print(prime(n))
