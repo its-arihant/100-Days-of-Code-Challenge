6e48c8e16547fde82ddd4bd9e223b8e487412921
18arihant <arihant18082003@gmail.com>
2023-06-20 19:00:46 +0530

    Day 39

---
+++ Day 39/39.py
@@ -0,0 +1,17 @@
+# Trailing zeroes in factorial
+def countzeroes(n):
+    res=0
+    i=5
+    while(i<=n):
+        res=res+n//i
+        i=i*5
+    return res
+
+a=int(input("Enter a number:"))
+print("The trailing zeroes in factorial of",a,"are:",countzeroes(a)) 
+
+# for finding the trailing zeroes we can simply count the number of 2's and 5's
+# The number of 5's will be less than the number of 2's
+# so we only count number of 5's
+
+# Time complexity=Theta(log(n))
