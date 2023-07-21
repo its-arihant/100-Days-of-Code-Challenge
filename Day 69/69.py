60c12eba8a9f279172ae696435429e401465a3f0
18arihant <arihant18082003@gmail.com>
2023-07-21 15:40:16 +0530

    Day 69

---
+++ Day 69/69.py
@@ -0,0 +1,18 @@
+# 412. Fizz Buzz
+
+class Solution():
+    def fizzBuzz(self, n):
+        l=[]
+        for i in range(1,n+1):
+            if i%3==0 and i%5==0:
+                l.append("FizzBuzz")
+            elif i%3==0:
+                l.append("Fizz")
+            elif i%5==0:
+                l.append("Buzz")
+            else:
+                l.append(str(i))
+        return l
+    
+a=Solution()
+print(a.fizzBuzz(3))
